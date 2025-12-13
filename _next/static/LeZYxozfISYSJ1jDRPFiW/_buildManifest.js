b35bb60b14b1b24195f9131662dce8f6c4dda1a6
kenza-ben-ayed <btazanka@gmail.com>
2025-12-14 00:38:01 +0100

    Add initial implementation of Turbopack runtime module

--- _next/static/LeZYxozfISYSJ1jDRPFiW/_buildManifest.js
+++ _next/static/LeZYxozfISYSJ1jDRPFiW/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/portfolio-web-kenza//_next/:path+",
+        "destination": "/portfolio-web-kenza/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
